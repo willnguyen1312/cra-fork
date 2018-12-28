10cee3d0bc122506a44047994af5aeaa8ca57deb
Nam Nguyen <nam.nguyen.node@gmail.com>
2018-12-28 11:59:42 +0700

    Add copy-webpack-plugin

--- packages/react-scripts/config/webpack.config.js
+++ packages/react-scripts/config/webpack.config.js
@@ -25,6 +25,7 @@ const InterpolateHtmlPlugin = require('react-dev-utils/InterpolateHtmlPlugin');
 const WorkboxWebpackPlugin = require('workbox-webpack-plugin');
 const WatchMissingNodeModulesPlugin = require('react-dev-utils/WatchMissingNodeModulesPlugin');
 // const ModuleScopePlugin = require('react-dev-utils/ModuleScopePlugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 const getCSSModuleLocalIdent = require('react-dev-utils/getCSSModuleLocalIdent');
 const paths = require('./paths');
 const getClientEnvironment = require('./env');
@@ -652,6 +653,12 @@ module.exports = function(webpackEnv) {
           silent: true,
           formatter: typescriptFormatter,
         }),
+      new CopyWebpackPlugin([
+        // copy public folder from core
+        { from: paths.appPublic, to: paths.appBuild, ignore: [paths.appHtml] },
+        // overice by its own
+        { from: paths.selfPublic, to: paths.appBuild, ignore: [paths.appHtml] },
+      ]),
     ].filter(Boolean),
     // Some libraries import Node modules but don't use them in the browser.
     // Tell Webpack to provide empty mocks for them so importing them works.
